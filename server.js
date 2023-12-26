fe5d600f5ae67b981fab71dd28735faf875e0759
dead doomed <igna.gallardo@duocuc.cl>
2023-12-26 16:58:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const app = express()
 const port = 3000;
 
 app.get('/', (req, res) => {
+  console.time('tiempo');
   fetch('https://www.taydaelectronics.com/catalogsearch/result/?q=10+OHM+1%2F4W+1%25+Metal+Film')
   .then(function (response) {return response.text();})
-  .then(function (html) {res.send(html.split("SKU")[1].substring(2).split("|")[0]);})
+  .then(function (html) {res.send(html.split("SKU")[1].substring(2).split("|")[0]);});
+
+  console.timeEnd('tiempo');
 });
   
 
