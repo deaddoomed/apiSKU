253c63b83c878f36d739e2c405bfb60d7c63fbe8
dead doomed <igna.gallardo@duocuc.cl>
2023-12-26 14:53:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ app.get('/', (req, res) => {
   fetch('https://www.taydaelectronics.com/catalogsearch/result/?q=10+OHM+1%2F4W+1%25+Metal+Film')
   .then(function (response) {return response.text();})
   .then(function (html) {res.send(html.split("SKU")[1].substring(2).split("|")[0]);})
-  //res.send('Welcome to my server!');
 });
   
 
